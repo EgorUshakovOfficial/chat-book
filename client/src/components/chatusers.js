1f891c33da6e8bc85384645d4ee61a493f025ab9
Egor Ushakov <egorushakov13@gmail.com>
2023-08-01 09:05:48 -0600

    Removed users

--- client/src/components/chatusers.js
+++ client/src/components/chatusers.js
@@ -2,10 +2,6 @@ import ChatSearch from './chatsearch';
 const ChatUsers = ({ users }) => {
     return (
         <ul id="chat-users">
-                <li className="chat-user">Egor Ushakov</li>
-                <li className="chat-user">Ilya Ushakov</li>
-                <li className="chat-user">Zoe Yu</li>
-                <li className="chat-user">Lebron James</li>
                 {users.map((user, i) => {
                     return <li className="chat-user" key={i}>{user.firstName} {user.lastName}</li>
                 })}
@@ -13,4 +9,4 @@ const ChatUsers = ({ users }) => {
     )
 }
 
-export default ChatUsers; 
+export default ChatUsers;
